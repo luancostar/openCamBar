9882d97e3d3e9ad62a2dd11e8ebaf766e43a2478
Moises Ferreira de Sousa <60903024+sousam02@users.noreply.github.com>
2023-04-25 13:09:48 -0300

    Refatoração de links

--- pages/volumes.php
+++ pages/volumes.php
@@ -1,109 +1,107 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/7a8d54eabc.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/coletas.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" ></script>
+    <link rel="stylesheet" href="../css/coletas.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>VB - Motoristas</title>
 </head>
+
 <body>
     <nav>
         <div class="nav-content">
             <div class="initial-nav">
-                <img src="img/logo_vb.png" alt="">
-                <p id="tag" >Motoristas</p>
+                <img src="../img/logo_vb.png" alt="">
+                <p id="tag">Motoristas</p>
             </div>
             <div class="final-nav">
                 <h6>João da Silva Bezerra <p>
-                    01305132583
-                </p></h6>
-                 
-                <img src="img/user.png" alt="">
+                        01305132583
+                    </p>
+                </h6>
+
+                <img src="../img/user.png" alt="">
                 <a href=""> <i class="fas fa-sign-out-alt">Sair</i> </a>
             </div>
         </div>
     </nav>
     <div class="container mt-5">
-	<div class="row mt-5">
-		<div class="col-12 col-lg-8">
-			<h1 style="font-size: 2rem !important; text-align: center;" class="display-4">Confirmação de Coleta
-            </h1>
-		</div>
-        <div id="camera"></div>
-        <form id="input-form" action="">
-            <label for="">Inserir Código de Barras</label>
-           <input id="resultado" value="" style="    width: 85%;
+        <div class="row mt-5">
+            <div class="col-12 col-lg-8">
+                <h1 style="font-size: 2rem !important; text-align: center;" class="display-4">Confirmação de Coleta
+                </h1>
+            </div>
+            <div id="camera"></div>
+            <form id="input-form" action="">
+                <label for="">Inserir Código de Barras</label>
+                <input id="resultado" value="" style="    width: 85%;
            border-radius: 5px;
            border: 1px solid;
            margin-bottom: 10px;
            text-align: center;
            font-size: 20px;
-           font-weight: bold;"   type="text"/>
-            <button style="background-color: #0070ff;
+           font-weight: bold;" type="text" />
+                <button style="background-color: #0070ff;
             width: 85%;
             border: none;
-            border-radius: 5px;" type="button" onclick="mostrarEsconderDiv()"><img style="width: 28px;" src="img/camera.png" alt=""></button>
-            
-           
-        </form>
-      
-        <div id="resultado"></div>
-        <script src="js/quagga.min.js"></script>
- 
-        <script>
-    
-            Quagga.init({
-                inputStream: {
-                    name: "Live",
-                    type: "LiveStream",
-                    target: document.querySelector('#camera')    // Or '#yourElement' (optional)
-                },
-                decoder: {
-                    readers: ["codabar_reader"]
-                }
-            }, function (err) {
-                if (err) {
-                    console.log(err);
-                    return
-                }
-                console.log("Initialization finished. Ready to start");
-                Quagga.start();
-            });
-    
-            Quagga.onDetected(function (data) {
+            border-radius: 5px;" type="button" onclick="mostrarEsconderDiv()"><img style="width: 28px;" src="../img/camera.png" alt=""></button>
 
-                console.log(trimmedCode);
 
-                var code = data.codeResult.code;
-                var trimmedCode = code.substring(1, code.length - 1);
-                document.getElementById('resultado').value = trimmedCode;
-                // document.getElementById('resultado').value = data.codeResult.code;
-           
-            });
-    
-        </script>
-        <script>
-        function mostrarEsconderDiv() {
-            var minhaDiv = document.getElementById("camera");
-            if (minhaDiv.style.display === "none") {
-                minhaDiv.style.display = "block";
-            } else {
-                minhaDiv.style.display = "none";
-            }
-            }
-        </script>
-        <style>
-            #camera {
-            display: none;
-            }
-        </style>
+            </form>
+
+            <div id="resultado"></div>
+            <script src="../js/quagga.min.js"></script>
+
+            <script>
+                Quagga.init({
+                    inputStream: {
+                        name: "Live",
+                        type: "LiveStream",
+                        target: document.querySelector('#camera') // Or '#yourElement' (optional)
+                    },
+                    decoder: {
+                        readers: ["codabar_reader"]
+                    }
+                }, function(err) {
+                    if (err) {
+                        console.log(err);
+                        return
+                    }
+                    console.log("Initialization finished. Ready to start");
+                    Quagga.start();
+                });
+
+                Quagga.onDetected(function(data) {
+
+                    console.log(trimmedCode);
+
+                    var code = data.codeResult.code;
+                    var trimmedCode = code.substring(1, code.length - 1);
+                    document.getElementById('resultado').value = trimmedCode;
+                    // document.getElementById('resultado').value = data.codeResult.code;
+
+                });
+            </script>
+            <script>
+                function mostrarEsconderDiv() {
+                    var minhaDiv = document.getElementById("camera");
+                    if (minhaDiv.style.display === "none") {
+                        minhaDiv.style.display = "block";
+                    } else {
+                        minhaDiv.style.display = "none";
+                    }
+                }
+            </script>
+            <style>
+                #camera {
+                    display: none;
+                }
+            </style>
 </body>
+
 </html>
-
-
-
-
